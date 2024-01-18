aafa208fb4e3eeb37e8e059ba055b4d259f0dfd2
Aiden <macuser@MacBook-Air.local>
2024-01-17 22:01:15 -0700

    changed image names

--- leaderboard.js
+++ leaderboard.js
@@ -2,7 +2,7 @@ const highScoresList = document.querySelector('#highScoresList')
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
 
-document.body.style.cssText = "background: navy;background-image: url(./assets/images/5073414.jpg);"
+document.body.style.cssText = "background: navy;background-image: url(./assets/images/hexImages.jpg);"
 document.querySelector(".container").style.cssText = "display: flex; justify-content: center";
 // document.querySelector("#highScores").style.cssText = "display: flex; justify-content: center; flex-direction: column; align-items: center;";
 document.querySelector(".leaderboard").style.cssText = "font-size: 4rem; color: red; margin: 40px auto; text-align: center;";
